fab886574c7a4f3cca924114975d8dcab5473073
Apprentice <apprentice@dbc-chicago-010.local>
2014-03-11 11:12:06 -0500

    added test to check for created_at timestamp on initialization

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,17 +1,21 @@
 describe Task do
-  let(:task) { Task.new(title: "travel", description: "by sailboat", timestamp: "'2014-04-01 00:00:01'") }
+  let(:task) { Task.new(title: "travel", description: "by sailboat") }
 
   context "#initialize" do
     it "creates a Task object" do
       task.should be_an_instance_of Task
     end
 
-    it "requires three parameters" do
+    it "requires parameters" do
       expect { Task.new }.to raise_error(ArgumentError)
     end
 
     it "requires default status to be incomplete" do
       expect(task.status).to eq "incomplete"
+    end
+
+    it "requires created_at timestamp should be automatically set on initialization" do
+      expect(task.created_at).to eq true
     end
 
   end
