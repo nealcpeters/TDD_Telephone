895f679ed30d18a6884d7b232eb82cadca4a8f7e
Apprentice <apprentice@dbc-chicago-010.local>
2014-03-11 10:56:59 -0500

    second challenge

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,13 +1,18 @@
 describe Task do
-  let(:task) { Task.new(title: "travel", description: "by sailboat", status: "later", timestamp: "'2014-04-01 00:00:01'") }
+  let(:task) { Task.new(title: "travel", description: "by sailboat", timestamp: "'2014-04-01 00:00:01'") }
 
   context "#initialize" do
     it "creates a Task object" do
       task.should be_an_instance_of Task
     end
 
-    it "requires four parameters" do
+    it "requires three parameters" do
       expect { Task.new }.to raise_error(ArgumentError)
     end
+
+    it "requires default status to be incomplete" do
+      expect { task.status }.to eq "incomplete"
+    end
+
   end
 end
